522df3fd3319592c6879686f2700add9ade81b23
andialfirqahtunnajiyahahsyar <firqhh.29@gmail.com>
2025-05-15 18:59:23 +0800

    Tugas foreach

---
+++ tugas12.php
@@ -0,0 +1,23 @@
+<?php
+
+$nama_kelas_10b = [
+    "Akifah Zahrani Arafat",
+    "Al Qumaerah",
+    "Althaf Azza Zahra",
+    "Andi AlFirqahtunnajiyah Ahsyar",
+    "Andi Mufrodatul Fanzila",
+    "Fahrah Furaidah Sahrir IPJ",
+    "Fauziyyah",
+    "Jilan Fathiyyah",
+    "Nadia Mirzayanti Hasbi",
+    "Rabi'atul Adawiyah",
+    "Alifah Izzah Rahmadini",
+]; 
+$nama_kelas_10b [] = "Muftihaturrahmah";
+$nama_kelas_10b [] = "Auliyah Nastiti Khayla Armada";
+$nama_kelas_10b [] = "Putri Kinanti Almagfirah";
+
+echo "Daftar Anggota Kelas 10B </br></br>" ;
+ foreach ($nama_kelas_10b as $anggota_kelas_10b) {
+    echo "-" . $anggota_kelas_10b . "</br>" ;
+ }
